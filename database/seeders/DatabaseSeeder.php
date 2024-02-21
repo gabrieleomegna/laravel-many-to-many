5b9d6579081e2950293ad9e0657fa0757eb1e319
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-21 20:15:59 +0100

    changed order of seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            ProjectSeeder::class,
             TypeSeeder::class,
+            ProjectSeeder::class,
+            TechnologySeeder::class,
+            ProjectTechnologySeeder::class
         ]);
     }
 }
